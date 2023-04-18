71d132d340c1045df9e6c0bfe213bbb7ddf67586
Nikita Sorokin <nstttrap.sab@gmail.com>
2023-04-18 10:37:21 +0300

    Добавлены необходимые картинки. Создан и определён класс UserInterfaceScreen.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,9 @@
-#include <SFML/Graphics.hpp>
-
-using namespace sf;
+#include <userinterfacescreen.h>
 
 int main()
 {
-    RenderWindow win(sf::VideoMode(400,400), "SFML");
-    win.setFramerateLimit(50);
-    while (win.isOpen()) {
-        Event ev;
-        while (win.pollEvent(ev)) {
-            if(ev.type == Event::Closed)
-                win.close();
-        }
-        win.clear();
-        win.display();
-    }
+    UserInterfaceScreen* uis = new UserInterfaceScreen();
+    uis->interact();
+    delete uis;
     return 0;
 }
